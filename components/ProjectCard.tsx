9c1c209a9ab11db196b97cd36cdd239a35046b07
Mamen <fbmaricarmen@gamil.com>
2024-04-06 23:17:43 +0200

    iconos

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -34,7 +34,7 @@ const ProjectCard = ({ image, title, text}: Props) => {
             className='w-full h-full group relative flip-card-front bg-cover bg-center text-red rounded-lg p-4'>
                     <div  className='absolute inset-0 w-full h-full rounded-md bg-red opacity-0 group-hover:opacity-40'/>
                     <div className='absolute inset-0 w-full h-full text-[20px] pb-10 hidden group-hover:flex items-center z-[20] justify-center text-purple-800 font-bold'>
-                      Sigue leyendo 👉
+                      👌😊 MÁS INFORMACIÓN 👉 
                     </div>
 
             </div>
