a77890cbd99cb388060050324d8ef7360f84da25
Mamen <fbmaricarmen@gmail.com>
2024-07-20 13:31:18 +0200

    linear-gradient

--- app/my-skills/page.tsx
+++ app/my-skills/page.tsx
@@ -6,39 +6,37 @@ import { SkillData } from "@/constants";
 import Image from "next/image";
 import { Autoplay } from "swiper/modules";
 
-
 const Page = () => {
   return (
     <div
       style={{ backgroundImage: "url(/bg-2.jpg)" }}
       className="h-screen w-screen flex items-center justify-center bg-cover bg-center"
     >
-     <div className="flex flex-col gap-10 px-4 md:gap-20 max-w-[90%] md:max-w-[80%] text-center items-center">
+      <div className="flex flex-col gap-10 px-4 md:gap-20 max-w-[90%] md:max-w-[80%] text-center items-center">
         <div className="flex flex-col items-center gap-4">
-          <h1 className="font-semibold text-white text-[10px] md:text-[40px]">
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-100 to-red-100 font-bold text-lg md:text-xl lg:text-2xl">
+          <h1 className="font-bold text-white text-[10px] md:text-[40px]">
+            <span className="text-[7px] md:text-[30px] lg:text-[40px]text-transparent bg-clip-text bg-gradient-to-r from-purple-100 to-red-100 font-bold text-lg md:text-xl lg:text-2xl">
               Rodeada de tecnología, busco capturar su esencia con pasión y curiosidad.
-     
-              <h2 className="text-[30px] text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-500 font-black text-sm sm:text-lg lg:text-xl xl:text-2xl">
-  Este es mi Stack Tecnológico
-</h2>
-         
-        
-            
             </span>
-          <Image
-          src="/mamen.png"
-          alt="Mamen"
-          height={100} 
-          width={100
-
-          } 
-          className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-[15] rounded-lg"
-        />
           </h1>
+          <h2 className="text-[12px] md:text-[30px] lg:text-[30px] text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-500 font-black">
+            Este es mi Stack Tecnológico
+          </h2>
           
+          <Image
+            src="/mamen.png"
+            alt="Mamen"
+            height={120}
+            width={120}
+            className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-[15] rounded-lg "
+            style={{
+              WebkitMaskImage: 'linear-gradient(to bottom, black 90%, transparent), linear-gradient(to right, black 90%, transparent)',
+              WebkitMaskComposite: 'source-in',
+              maskImage: 'linear-gradient(to bottom, black 90%, transparent), linear-gradient(to right, black 90%, transparent)',
+              maskComposite: 'intersect'
+            }}
+          />
         </div>
-        
         <Swiper
           slidesPerView={5}
           loop={true}
@@ -61,7 +59,6 @@ const Page = () => {
             </SwiperSlide>
           ))}
         </Swiper>
-        
         <Swiper
           slidesPerView={5}
           loop={true}
