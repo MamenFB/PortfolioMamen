d6d9e5b1383849c4c24810fc5a4f3d7f0cf3a96d
Mamen <fbmaricarmen@gmail.com>
2024-07-20 13:47:25 +0200

    logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,6 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Portfolio Mamen',
   description: 'MAMEN',
-  
-  
 }
 
 export default function RootLayout({
@@ -20,11 +18,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" sizes="16
+        x16" href="/logo3r.ico" />
+      </head>
       <body className={inter.className}>
         <Navbar />
         {children}
         <Navigation />
-        </body>
+      </body>
     </html>
   )
 }
